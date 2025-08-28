6108c0b550c998d922c6299fba2be176941f3406
Shankar Chakraborty <shankarxdev@gmail.com>
2025-08-28 18:52:43 -0400

    refactor

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('my-first-app');
